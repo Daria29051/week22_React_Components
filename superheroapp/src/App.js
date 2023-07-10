8dd8cdf0d5f0dc933d833144f78a35582ca9a7e8
unknown <daria.lantyugova@mail.ru>
2023-07-11 00:06:17 +0300

    superheroes 3rd version

--- superheroapp/src/App.js
+++ superheroapp/src/App.js
@@ -117,7 +117,7 @@ function App() {
   return (
     <div className="App">
       {superheroes.map((superhero) => (
-        <Superhero name={superhero.name} universe={superhero.universe} alterego={superhero.alterego} occupation={superhero.occupation} friends={superhero.friends} superpowers={superhero.superpowers} url={superhero.url} ></Superhero>
+        <Superhero name={superhero.name} universe={superhero.universe} alterego={superhero.alterego} occupation={superhero.occupation} friends={superhero.friends} superpowers={superhero.superpowers} url={superhero.url} info={superhero.info} ></Superhero>
       ))}
     </div>
   );
--- superheroapp/src/components/Superhero.jsx
+++ superheroapp/src/components/Superhero.jsx
@@ -7,13 +7,14 @@ function Superhero(props) {
 <React.Fragment>
 <div className="hero">
         <h1 className="hero__name">{props.name}</h1>
-        <div className="hero__charactetistic hero__universe"><span className="hero__subtitle">Вселенная:</span> {props.universe}</div>
+        <div className="hero__charactetistic hero__universe">{props.universe}</div>
         <div className="hero__charactetistic hero__alterego"><span className="hero__subtitle">Альтер эго:</span> {props.alterego}</div>
         <div className="hero__charactetistic hero__occupation"><span className="hero__subtitle">Род деятельности:</span> {props.occupation}</div>
         <div className="hero__charactetistic hero__friends"><span className="hero__subtitle">Друзья:</span> {props.friends}</div>
         <div className="hero__charactetistic hero__superpowers"><span className="hero__subtitle">Суперсилы:</span> {props.superpowers}</div>
+        <div className="hero__info"><span className="hero__subtitle">О герое:</span> {props.info}</div>
         <img className="hero__img"src={props.url} alt="hero image"/>
-        <button className="hero__button"> О герое</button>
+        <button className="hero__button">Подробнее</button>
         </div>
 </React.Fragment>
 
